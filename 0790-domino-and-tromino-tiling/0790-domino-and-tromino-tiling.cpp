d1cd3e50ded2aed0022cc138d8192a3725e60f54
mkkhastagir20-sketch <mkkhastagir20@gmail.com>
2026-07-01 13:46:17 +0530

    Time: 0 ms (100%), Space: 8 MB (79.91%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int t[1001];
+    int M =1000000007;
+int solve(int n){
+       
+       if(t[n]!=-1){
+        return t[n];
+       }
+       
+       
+       
+        if (n==1||n==2){
+            return n;
+        }
+        if (n==3){
+        return 5;
+        }
+return t[n] = (2*solve(n-1)%M+ solve(n-3)%M)%M; 
+
+
+}
+     int numTilings(int n) {
+        memset(t,-1,sizeof(t));
+        return solve(n);
+    }
+};
